9f1af8a0a23c4753215c8d2f399f46df4a408411
Manan Arora <mananarora313@gmail.com>
2024-09-02 13:15:07 -0500

    added Assignments folder

---
+++ Assignments/mxa3328_assmt1/expense_8_puzzle.py
@@ -0,0 +1,10 @@
+# this is a modified version of the 8 puzzle problem implemented by Manan Arora (mxa3328)
+
+import sys
+
+class expense_8_puzzle:
+    def main():
+        print(sys.argv)
+
+if __name__ == "__main__":
+    expense_8_puzzle.main()
