d5c210d2d96953ca8ca96d30f357d74cb7d42411
manan05 <mananarorauta@gmail.com>
2024-09-08 01:58:28 -0500

    added BFS full code

--- Assignments/mxa3328_assmt1/expense_8_puzzle.py
+++ Assignments/mxa3328_assmt1/expense_8_puzzle.py
@@ -16,43 +16,95 @@ class expense_8_puzzle:
                 puzzle.append([int(num) for num in i.split()])
         return puzzle
 
+    # finding zero (empty place)
+    def find_zero(self, puzzle):
+        for i in range(3):
+            for j in range(3):
+                if (puzzle[i][j] == 0):
+                    return i, j
+
+    # checking the valid moves from that particular position in puzzle
     def valid_moves(self, puzzle):
         moves = []
 
-        # finding zero (empty place)
-        for i in range(len(puzzle)):
-            for j in range(len(puzzle[0])):
-                if (puzzle[i][j] == 0):
-                    x, y = i, j
-
+        x, y = self.find_zero(puzzle=puzzle)
         # if zero is in 1st row or 2nd row it can move UP but not if it is in 0th row
         if (x > 0):
-            moves.append((x-1, y, "up"))
+            moves.append((x-1, y, "down"))
 
         # if zero is in 0th row or 1st row it can move DOWN but not if it is in 2nd row
         if (x < 2):
-            moves.append((x + 1, y, "down"))
+            moves.append((x + 1, y, "up"))
 
         # if zero is in 1st column or 2nd column it can move LEFT but not if it is in 0th column
         if (y > 0):
-            moves.append((x, y - 1, "left"))
+            moves.append((x, y - 1, "right"))
 
         # if zero is in 0th column or 1st column it can move RIGHT but not if it is in 2nd column
         if (y < 2):
-            moves.append((x, y + 1, "right"))
-            
+            moves.append((x, y + 1, "left"))
+
         return moves
 
-    def bfs(self, start_state, goal_state, dump_flag):
+    # applies a move to the puzzle state and returns the new state
+    def move(self, puzzle, move):
+        x, y = self.find_zero(puzzle=puzzle)
+        after_move_puzzle = [row[:] for row in puzzle]
+        new_x, new_y, direction = move
+
+        tile_moved = after_move_puzzle[new_x][new_y]
+        after_move_puzzle[x][y], after_move_puzzle[new_x][new_y] = after_move_puzzle[new_x][new_y], after_move_puzzle[x][y]
+
+        return after_move_puzzle, direction, tile_moved
+
+    def bfs(self, start_state_file, goal_state_file, dump_flag):
         # Breadth First Search implementation
-        start_puzzle = self.read_state(start_state)
-        end_puzzle = self.read_state(goal_state)
 
-        print(self.valid_moves(start_puzzle))
+        # Reading the start and goal state of the puzzle
+        start_state = self.read_state(start_state_file)
+        goal_state = self.read_state(goal_state_file)
 
-        # print("bfs implementation")
-        print("start_puzzle", start_puzzle)
-        # print("end_puzzle", end_puzzle)
+        # Queue holds (current_state, path taken, total cost)
+        queue = deque([(start_state, [], 0)])
+
+        visited = set()  # To store visited states
+        visited.add(tuple(tuple(row) for row in start_state))
+
+        nodes_popped = 0
+        nodes_expanded = 0
+        max_fringe_size = 0
+
+        while queue:
+            max_fringe_size = max(max_fringe_size, len(queue))
+            current_state, path, total_cost = queue.popleft()
+            nodes_popped += 1
+
+            # Check if the current state matches the goal state
+            if current_state == goal_state:
+                print("Nodes Popped: ", nodes_popped)
+                print("Nodes Expanded: ", nodes_expanded)
+                print("Max Fringe Size: ", max_fringe_size)
+                print(
+                    f"Solution found at depth: {len(path)} with cost of {total_cost}")
+                print("Steps: ")
+                for i in path:
+                    print(f"Move {i[0]} {i[1]}")
+                return
+
+            # Explore valid moves
+            for new_move in self.valid_moves(puzzle=current_state):
+                new_state, direction, cost = self.move(
+                    puzzle=current_state, move=new_move)
+                state_tuple = tuple(tuple(row) for row in new_state)
+
+                # If the new state hasn't been visited yet
+                if state_tuple not in visited:
+                    visited.add(state_tuple)
+                    queue.append(
+                        (new_state, path + [(cost, direction)], total_cost + cost))
+                    nodes_expanded += 1
+
+        print("No solution found.")
 
     def ucs(self, start_state, goal_state, dump_flag):
         # uniform cost search implementation
@@ -128,19 +180,26 @@ def main():
     print("dump_flag: ", dump_flag)
 
     if method == "bfs":
-        solution.bfs(start_state, goal_state, dump_flag)
+        solution.bfs(start_state_file=start_state,
+                     goal_state_file=goal_state, dump_flag=dump_flag)
     elif method == "ucs":
-        solution.ucs(start_state, goal_state, dump_flag)
+        solution.ucs(start_state=start_state,
+                     goal_state=goal_state, dump_flag=dump_flag)
     elif method == "dfs":
-        solution.dfs(start_state, goal_state, dump_flag)
+        solution.dfs(start_state=start_state,
+                     goal_state=goal_state, dump_flag=dump_flag)
     elif method == "dls":
-        solution.dls(start_state, goal_state, dump_flag)
+        solution.dls(start_state=start_state,
+                     goal_state=goal_state, dump_flag=dump_flag)
     elif method == "ids":
-        solution.ids(start_state, goal_state, dump_flag)
+        solution.ids(start_state=start_state,
+                     goal_state=goal_state, dump_flag=dump_flag)
     elif method == "greedy":
-        solution.greedy(start_state, goal_state, dump_flag)
+        solution.greedy(start_state=start_state,
+                        goal_state=goal_state, dump_flag=dump_flag)
     elif method == "a*":
-        solution.a_star(start_state, goal_state, dump_flag)
+        solution.a_star(start_state=start_state,
+                        goal_state=goal_state, dump_flag=dump_flag)
     else:
         print("Please check the inputs arguments and try again...")
         return
